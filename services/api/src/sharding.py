40ac4464f0b54fe92320d937162dd041d52dc64d
Juan Fernando Plata <juanfernandoplata@gmail.com>
2024-10-13 13:18:52 -0500

    - Migrated client.py (previously a toy client) to a FastAPI app. - Renamed client.py to main.py following FastAPI convention.

--- services/api/src/sharding.py
+++ services/api/src/sharding.py
@@ -1,17 +1,20 @@
-import psycopg as pg
+from psycopg_pool import AsyncConnectionPool
 
 class PgShardManager:
     def __init__( self, conn_dict, shard_selector ):
-        self.conn_dit = conn_dict
+        self.conn_dict = conn_dict
         self.shard_selector = shard_selector
     
     async def open( self ):
         self.pools = dict()
 
-        for key, string in self.conn_dict:
-            self.pools[ key ] = pg.AsyncConnectionPool( string, open = False )
+        for key, string in self.conn_dict.items():
+            self.pools[ key ] = AsyncConnectionPool( string, open = False )
             await self.pools[ key ].open()
+    
+    async def close( self ):
+        for key, _ in self.conn_dict.items():
+            await self.pools[ key ].close()
 
-
-    async def connection( self, key ):
+    def connection( self, key ):
         return self.pools[ self.shard_selector( key ) ].connection()
