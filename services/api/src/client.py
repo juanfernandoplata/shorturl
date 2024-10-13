db89ce23b29ccbf801294c4129b9866481fe1a16
Juan Fernando Plata <juanfernandoplata@gmail.com>
2024-10-13 08:57:38 -0500

    - Added SSL/TSL support for client.

--- services/api/src/client.py
+++ services/api/src/client.py
@@ -2,12 +2,28 @@ import grpc
 from _grpc.url_encoder_pb2_grpc import UrlEncoderStub
 from _grpc.url_encoder_pb2 import EncodeReq
 
-import tsl
+from _grpc import tsl
+
+from os import environ
+
+from sharding import PgShardManager
 
 from time import sleep
 
 if __name__ == "__main__":
-    sleep( 5 )
+    if ( conn_strings := environ.get( "CONN_STRINGS" ) ) == None:
+        print( "FATAL: environment variable CONN_STRINGS was not found" )
+        exit()
+    
+    conn_dict = dict( zip(
+        [ f"pg{i}" for i in range( 1, len( conn_strings ) ) ],
+        conn_strings
+    ))
+
+    dbs = PgShardManager(
+        conn_dict,
+        lambda key: "pg1" if key[ -1 ] < 86 else "pg2"
+    )
 
     credentials = grpc.ssl_channel_credentials(
         root_certificates = tsl.ROOT_CERTIFICATES,
@@ -16,7 +32,14 @@ if __name__ == "__main__":
     )
 
     client = UrlEncoderStub(
-        grpc.secure_channel( "localhost:50051", credentials )
+        grpc.secure_channel( "url-encoder:50051", credentials )
     )
 
-    print( client.encode( EncodeReq() ).encoded )
+    i = 1
+
+    while( True ):
+        sleep( 0.25 )
+
+        print( f"Response {i}: {client.encode( EncodeReq() ).encoded}" )
+
+        i += 1
