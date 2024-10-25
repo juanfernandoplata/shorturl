dc1019b5744c30aef7139cfb84b634c0785febef
Juan Fernando Plata <juanfernandoplata@gmail.com>
2024-10-25 11:10:25 -0500

    - Created dao folder for dao definitions. - These will be used for interacting with pg database, redis cache (not   yet implemented) and local cache.

---
+++ services/api/src/dao/__init__.py
@@ -0,0 +1,84 @@
+from typing import Tuple
+
+from abc import ABC, abstractmethod
+
+from ..sharding import PgShardManager
+from ..cache import LFULRUCache
+
+from .. import base62
+
+from fastapi import HTTPException
+
+class PgShardDao():
+    def __init__( self, shard_mannager: PgShardManager ):
+        self.shard_mannager = shard_mannager
+    
+class LFULRUCacheDao():
+    def __init__( self, cache: LFULRUCache ):
+        self.cache = cache
+
+class SUrlDao( ABC ):
+    @abstractmethod
+    def get( self, short: str ) -> str:
+        pass
+
+    @abstractmethod
+    def set( self, short: str, long: str ):
+        pass
+
+class PgShardSurlDao( PgShardDao, SUrlDao ):
+    def __init__( self ):
+        super().__init__()
+        self.to_set = dict()
+
+    async def reserve( self, long: str ):
+        db = None
+        url_id = None
+        short = None
+
+        db, connection = self.shard_mannager.balance( ret_db = True )
+
+        async with connection as conn:
+            curr = await conn.execute( f"""
+                insert into url( long )
+                values( '{ long }' )
+                returning url_id
+            """ )
+
+            url_id = ( await curr.fetchone() )[ 0 ]
+
+            short = base62.encode( url_id )
+        
+        self.to_set[ short ] = ( db, url_id )
+    
+        return short
+
+    async def get( self, short: str ) -> str:
+        async with self.shard_mannager.find( short ) as conn:
+            curr = await conn.execute( f"""
+                select long
+                from url
+                where short = '{ short }'
+            """ )
+
+            if ( long := ( await curr.fetchone() ) ) == None:
+                raise HTTPException(
+                    status_code = 404,
+                    detail = "URL was not found"
+                )
+            
+            return long[ 0 ]
+
+    async def set( self, short: str, long: str ):
+        if ( aux := self.to_set[ short ] ) == None:
+            print( "FATAL: this sould never print!!!" )
+            exit()
+        
+        db, url_id = aux
+
+        async with self.shard_mannager.select( db ) as conn:
+            await conn.execute( f"""
+                update url set
+                short = '{ short }'
+                where url_id = { url_id }
+            """ )
